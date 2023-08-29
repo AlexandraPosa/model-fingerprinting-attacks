92d9de4828835d29536e42e7a9533d18bd73142c
AlexandraPosa <alexandra.posa12@gmail.com>
2023-08-29 15:08:26 +0200

    correction

--- attacks/pruning_attack.py
+++ attacks/pruning_attack.py
@@ -34,10 +34,6 @@ np.random.seed(seed_value)
 tf.random.set_seed(seed_value)
 os.environ['PYTHONHASHSEED'] = str(seed_value)
 
-# set the random seed for GPU (if using one)
-if tf.config.experimental.list_physical_devices('GPU'):
-    tf.config.experimental.set_seed(seed_value)
-
 # register the custom regularizer to load the pre-trained model
 tf_utils.get_custom_objects()['FingerprintRegularizer'] = FingerprintRegularizer
 
