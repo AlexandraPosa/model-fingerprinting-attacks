87a78ea15db9c1eb69650d424afb43dd58cb0933
AlexandraPosa <alexandra.posa12@gmail.com>
2023-09-20 15:28:28 +0200

    Evaluate TFLite model performance.

--- attacks/quantization.py
+++ attacks/quantization.py
@@ -71,92 +71,100 @@ validation_output = test_output[train_size:]
 # ---------------------------------------- Quantization Aware Training -------------------------------------------------
 
 # create a quantization aware model
-quantized_model = quantize_model(pretrained_model)
+quant_aware_model = quantize_model(pretrained_model)
 
 # `quantize_model` requires a recompile.
-sgd = SGD(lr=0.001, momentum=0.9, nesterov=True)
-quantized_model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
-quantized_model.summary()
+sgd = SGD(lr=0.001,
+          momentum=0.9,
+          nesterov=True)
+quant_aware_model.compile(loss="categorical_crossentropy",
+                          optimizer=sgd,
+                          metrics=["accuracy"])
+quant_aware_model.summary()
 
 # fine-tuning the quantized model
 print("\nPerforming quantization aware training:")
-quantized_model.fit(training_input,
-                    training_output,
-                    batch_size=500,
-                    epochs=1,
-                    validation_split=0.2)
-
-# check the type of the quantized weights
-for layer in quantized_model.layers:
+quant_aware_model.fit(training_input,
+                      training_output,
+                      batch_size=500,
+                      epochs=1,
+                      validation_split=0.2)
+'''
+# check the data type of the quantized weights
+for layer in quant_aware_model.layers:
     for weight in layer.weights:
         if weight.dtype != 'float32':
             print(f"Layer {layer.name} has weights with data type {weight.dtype}")
-
+'''
 
 # check quantized model accuracy
 _, baseline_model_accuracy = pretrained_model.evaluate(validation_input,
                                                        validation_output,
                                                        verbose=0)
 
-_, quantized_model_accuracy = quantized_model.evaluate(validation_input,
-                                                       validation_output,
-                                                       verbose=0)
+_, quant_aware_model_accuracy = quant_aware_model.evaluate(validation_input,
+                                                           validation_output,
+                                                           verbose=0)
 
-print('Baseline test accuracy:', baseline_model_accuracy * 100)
-print('Quantized test accuracy:', quantized_model_accuracy * 100)
+print('Baseline model accuracy:', baseline_model_accuracy * 100)
+print('Quantization aware model accuracy:', quant_aware_model_accuracy * 100)
 
 # -------------------------------------- TF Lite Model Evaluation ------------------------------------------------------
 
 # create quantized model for TFLite backend
-converter = tf.lite.TFLiteConverter.from_keras_model(quantized_model)
+converter = tf.lite.TFLiteConverter.from_keras_model(quant_aware_model)
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 quantized_tflite_model = converter.convert()
 
 # load the TFLite model
 interpreter = tf.lite.Interpreter(model_content=quantized_tflite_model)
 interpreter.allocate_tensors()
-
+'''
 # check the data type of each tensor in the model
 for tensor_details in interpreter.get_tensor_details():
     tensor_name = tensor_details["name"]
     tensor_dtype = tensor_details["dtype"]
     print(f"Tensor {tensor_name} has dtype {tensor_dtype}.")
-
+'''
+# TFLite model evaluation function
 def evaluate_model(interpreter):
-  input_index = interpreter.get_input_details()[0]["index"]
-  output_index = interpreter.get_output_details()[0]["index"]
+    input_index = interpreter.get_input_details()[0]["index"]
+    output_index = interpreter.get_output_details()[0]["index"]
 
-  # Run predictions on every image in the "test" dataset.
-  prediction_digits = []
-  for i, test_image in enumerate(validation_input):
-    if i % 1000 == 0:
-      print('Evaluated on {n} results so far.'.format(n=i))
-    # Pre-processing: add batch dimension and convert to float32 to match with
-    # the model's input data format.
-    test_image = np.expand_dims(test_image, axis=0).astype(np.float32)
-    interpreter.set_tensor(input_index, test_image)
+    # run predictions on every image in the validation dataset
+    prediction_digits = []
+    for i, test_image in enumerate(validation_input):
+        if i % 1000 == 0:
+          print('Evaluated on {n} results so far.'.format(n=i))
 
-    # Run inference.
-    interpreter.invoke()
+        # pre-processing: add batch dimension and convert to float32 to match with the model's input data format
+        test_image = np.expand_dims(test_image, axis=0).astype(np.float32)
+        interpreter.set_tensor(input_index, test_image)
 
-    # Post-processing: remove batch dimension and find the digit with highest
-    # probability.
-    output = interpreter.tensor(output_index)
-    digit = np.argmax(output()[0])
-    prediction_digits.append(digit)
+        # run inference
+        interpreter.invoke()
 
-  print('\n')
-  # Compare prediction results with ground truth labels to calculate accuracy.
-  prediction_digits = np.array(prediction_digits)
-  accuracy = (prediction_digits == validation_output).mean()
-  return accuracy
+        # post-processing: remove batch dimension and find the digit with the highest probability
+        output = interpreter.tensor(output_index)
+        digit = np.argmax(output()[0])
+        prediction_digits.append(digit)
+
+    # compare prediction results with ground truth labels to calculate accuracy
+    predicted_labels = np.array(prediction_digits)
+    ground_truth_labels = np.argmax(validation_output, axis=1)  # convert one-hot encoded labels to single labels
+    accuracy = (predicted_labels == ground_truth_labels).astype(np.float32).mean()
+
+    return accuracy
 
 test_accuracy = evaluate_model(interpreter)
 
-print('Quant TFLite test_accuracy:', test_accuracy)
-print('Quant TF test accuracy:', quantized_model_accuracy)
+print('Quantized TFLite model accuracy:', test_accuracy * 100)
+print('Quantization aware model accuracy:', quant_aware_model_accuracy * 100)
 
-
+# save TFLite model to file
+tflite_model_path = os.path.join('result_09', 'quantized_model.tflite')
+with open(tflite_model_path, 'wb') as f:
+    f.write(quantized_tflite_model)
 
 
 
